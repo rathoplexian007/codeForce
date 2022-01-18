16522d20047abd2a6460fad263d08cdc03bda320
Quasilius-Starlord <rathoplexian007@gmail.com>
2022-01-18 16:50:24 +0530

    additional form data

--- src/components/KYCForm/KYCForm.js
+++ src/components/KYCForm/KYCForm.js
@@ -8,6 +8,12 @@ export default function KYCForm(props){
 
     const [ aadharFile, setAadharFile ] = useState(null);
     const [ PANFile, setPANFile ] = useState(null);
+    const [ name, setName ] = useState('');
+    const [ fatherName, setFatherName ] = useState('');
+    const [ motherName, setMotherName ] = useState('');
+    const [ address, setAddress ] = useState('');
+    const [ number, setNumber ] = useState('');
+    const [ DOB, setDOB ] = useState('');
     let submitHandler=e => {
         e.preventDefault();
         if(aadharFile!==null && PANFile!==null){
@@ -23,9 +29,38 @@ export default function KYCForm(props){
         }
     }
 
+    //name
+    //father name
+    //mother name
+    // address
+    // dob
     return(
         <div style={{width:'50%'}}>
             <Form onSubmit={e => submitHandler(e) }>
+                <Form.Group className="mb-3">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control value={name} type='text' onChange={e => {console.log(e.target.value);setName(e.target.value); }}/>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Fathter Name</Form.Label>
+                    <Form.Control value={fatherName} type='text' onChange={e => {console.log(e.target.value);setFatherName(e.target.value); }}/>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Mother Name</Form.Label>
+                    <Form.Control value={motherName} type='text' onChange={e => {console.log(e.target.value);setMotherName(e.target.value); }}/>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control value={address} type='text' onChange={e => {console.log(e.target.value);setAddress(e.target.value); }}/>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Contact Number</Form.Label>
+                    <Form.Control value={number} type='text' onChange={e => {console.log(e.target.value);setNumber(e.target.value); }}/>
+                </Form.Group>
+                <Form.Group className="mb-3">
+                    <Form.Label>Date of Birth</Form.Label>
+                    <Form.Control type='date' value={DOB} onChange={e => {console.log(e.target.value);setDOB(e.target.value); }}/>
+                </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Aadhar Card</Form.Label>
                     <Form.Control type='file' accept='.pdf' onChange={e => {console.log(e.target.files[0]);setAadharFile(e.target.files[0]); }}/>
